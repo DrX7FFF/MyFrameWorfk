59ae833ff67f6043fa11c4738ff457dd716ba23e
DrX7FFF <dubourg.v@gmail.com>
2022-05-09 22:27:15 +0200

    Add MySmartConfig

--- src/myfunction.h
+++ src/myfunction.h
@@ -1,1 +1,16 @@
+#include <Arduino.h>
+#include <WiFi.h>
 #define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
+
+void mySmartConfig(){
+	if (WiFi.begin() != wl_status_t::WL_DISCONNECTED){
+		DEBUGLOG("Start SmartConfig :\n");
+		WiFi.mode(WIFI_AP_STA);
+		WiFi.beginSmartConfig();		
+		while (!WiFi.smartConfigDone()){
+			delay(100);
+			DEBUGLOG("/");
+		}
+		ESP.restart();
+	}
+}
